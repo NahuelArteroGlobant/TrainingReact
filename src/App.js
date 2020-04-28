2b4dad7d9a81bf762b1d125fa31a3e240b317ed2
Nahuel Miguel Artero <nahuel.artero@globant.com>
2020-04-27 21:37:35 -0300

    primera parte del proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "../src/images/logo1.png";
+import logo2 from "../src/images/newsAvatar.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="Home__header">
+          <img src={logo} alt="Logo " />
+          <h1>Diario Digital</h1>
+        </div>
+        <div className="Home__section-name">
+          <img className="Home__avatar" src={logo2} alt="noticias" />
+          <h1>nombre del periodista</h1>
+        </div>
+        <div className="Home__section-info">
+          <p>diario en formato digital</p>
+          <p>bla bla bla</p>
+        </div>
+        <div className="Home__footer"> #NoticiasDeHoy</div>
+      </div>
+    );
+  }
 }
-
 export default App;
